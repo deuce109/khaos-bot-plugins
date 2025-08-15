0db433f5ad752505717de07ff2acb6f6a1e0d170
Dalton Edmsiten <daltonedmisten@gmail.com>
2025-08-15 11:25:38 -0500

    udpated to show size with labels made files read out in code block added requests to requirements.txt for server plugin

--- read_files.py
+++ read_files.py
@@ -2,13 +2,21 @@ from discord import Attachment
 
 COMMAND = "files"
 
+def _determine_size(size_in_bytes: int) -> str:
+    size_labels = ["B", "KB", "MB", "GB"]
+    counter = 0
+    size = size_in_bytes
+    while size > 1024:
+        size = size / 1024.0
+        counter += 1 
+        
+    return f"{size}{size_labels[counter]}"
+
 def execute(_: list[str], attachments: list[Attachment]) -> str:
     
     output = ""
     
     for attach in attachments:
-        output += f"{attach.filename} Length: {attach.size}\n"
+        output += f"`{attach.filename}` Length: {_determine_size(attach.size)}\n"
         
     return output
-        
-        
